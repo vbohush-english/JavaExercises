18f042d4ac83ea5479e57dad4dae632ce848227d
vbohush <vbohush@server.fake>
2013-12-03 17:21:11 +0200

    changed output

--- src/ua/pp/viktor/chapter01/Exercise07.java
+++ src/ua/pp/viktor/chapter01/Exercise07.java
@@ -3,8 +3,8 @@ package ua.pp.viktor.chapter01;
 public class Exercise07 {
 
 	public static void main(String[] args) {
-		System.out.println(4.0 * (1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0 + 1.0 / 9.0 - 1.0 / 11.0));
-		System.out.println(4.0 * (1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0 + 1.0 / 9.0 - 1.0 / 11.0 + 1.0 / 13.0));
+		System.out.println("Pi = " + (4.0 * (1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0 + 1.0 / 9.0 - 1.0 / 11.0)));
+		System.out.println("Pi = " + (4.0 * (1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0 + 1.0 / 9.0 - 1.0 / 11.0 + 1.0 / 13.0)));
 	}
 
 }
