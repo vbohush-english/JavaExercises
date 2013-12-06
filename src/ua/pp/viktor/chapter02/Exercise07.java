e92ee1f1d023c5d46ea6f1146d6aacce2e02b113
Viktor Bohush <vbohush@ukr.net>
2013-12-06 11:47:44 +0200

    solve 2.5-2.9

---
+++ src/ua/pp/viktor/chapter02/Exercise07.java
@@ -0,0 +1,19 @@
+package ua.pp.viktor.chapter02;
+
+import java.util.Scanner;
+
+public class Exercise07 {
+
+	public static void main(String[] args) {
+		System.out.print("Enter the number of minutes: ");
+		Scanner input = new Scanner(System.in);
+		int minutes = input.nextInt();
+		input.close();
+		int minutesInDay = 60 * 24;
+		int minutesInYear = minutesInDay * 365;
+		int years =  minutes / minutesInYear;
+		int days = (minutes % minutesInYear) / minutesInDay;
+		System.out.println(minutes + " minutes is approximately " + years + " years and " + days);
+	}
+
+}
