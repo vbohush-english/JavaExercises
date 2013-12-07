f9b4336b71c63c825259603e6ed6119fb1bae229
Viktor Bohush <vbohush@ukr.net>
2013-12-07 16:52:29 +0200

    solve exercises 3.2 - 3.5

---
+++ src/ua/pp/viktor/chapter03/Exercise02.java
@@ -0,0 +1,20 @@
+package ua.pp.viktor.chapter03;
+
+import java.util.Scanner;
+
+public class Exercise02 {
+
+	public static void main(String[] args) {
+		int number1 = (int)(Math.random() * 10);
+		int number2 = (int)(Math.random() * 10);
+		int number3 = (int)(Math.random() * 10);
+		Scanner input = new Scanner(System.in);
+		System.out.print("What is " + number1 + " + " + number2 + " + " + number3 + "? ");
+		int answer = input.nextInt();
+		input.close();
+		System.out.println(number1 + " + " + number2 + " + "+ number3 + 
+				" = " + answer + " is " + 
+		    	(number1 + number2 + number3 == answer));
+	}
+
+}
