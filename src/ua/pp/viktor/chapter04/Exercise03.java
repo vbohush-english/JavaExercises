7ae39453935b4476ef5955f8ee716e087ab36faf
Viktor Bohush <vbohush@server.fake>
2013-12-09 21:47:43 +0200

    solve exercises 4.2 - 4.6

---
+++ src/ua/pp/viktor/chapter04/Exercise03.java
@@ -0,0 +1,12 @@
+package ua.pp.viktor.chapter04;
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		System.out.println("Kilograms\tPounds");
+		for (int kilograms = 1; kilograms < 200; kilograms += 2) {
+			System.out.println(kilograms + "\t\ts" + (int)(kilograms * 2.2 * 10) / 10.0);	
+		}
+	}
+
+}
---
+++ src/ua/pp/viktor/chapter04/Exercise04.java
@@ -0,0 +1,12 @@
+package ua.pp.viktor.chapter04;
+
+public class Exercise04 {
+
+	public static void main(String[] args) {
+		System.out.println("Miles\tKilometers");
+		for (int miles = 1; miles <= 10; miles++) {
+			System.out.println(miles + "\t" + miles * 1.609);	
+		}
+	}
+
+}
