a0ac0bc9472df1fc21ebae689f161af2775118bc
Viktor Bohush <vbohush@server.fake>
2013-12-12 19:10:22 +0200

    solve exercises 4.35 - 4.37

---
+++ src/ua/pp/viktor/chapter04/Exercise35.java
@@ -0,0 +1,13 @@
+package ua.pp.viktor.chapter04;
+
+public class Exercise35 {
+
+	public static void main(String[] args) {
+		double sum = 1 / (1 + Math.sqrt(2));
+		for (int i = 2; i <=624; i++) {
+			sum += 1 / (Math.sqrt(i) + Math.sqrt(i + 1));
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ src/ua/pp/viktor/chapter04/Exercise36.java
@@ -0,0 +1,23 @@
+package ua.pp.viktor.chapter04;
+
+import java.util.Scanner;
+
+public class Exercise36 {
+
+	public static void main(String[] args) {
+		System.out.print("Enter the first 9 digits of an ISBN as integer: ");
+		Scanner input = new Scanner(System.in);
+		int isbn = input.nextInt();
+		input.close();
+		int d10 = 0;
+		System.out.print("The ISBN-10 number is ");
+		for (int i = 1, divisor = 100000000; i < 10; i++, divisor /= 10) {
+			int tmp = isbn / divisor;
+			d10 += tmp * i;
+			isbn %= divisor;
+			System.out.print(tmp);
+		}		
+		System.out.println(d10 % 11 == 10 ? "X" : d10 % 11);
+	}
+
+}
