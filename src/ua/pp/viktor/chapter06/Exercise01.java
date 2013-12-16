2898c2891522744b10caa7894a67ff2f54add3df
Viktor Bohush <vbohush@server.fake>
2013-12-16 19:00:36 +0200

    solve exercises 6.1 - 6.2

---
+++ src/ua/pp/viktor/chapter06/Exercise01.java
@@ -0,0 +1,39 @@
+package ua.pp.viktor.chapter06;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the number of students: ");		
+		int numberOfStudents = input.nextInt();
+		System.out.print("Enter " + numberOfStudents + " scores: ");	
+		int[] students = new int[numberOfStudents];
+		int best = 0;
+		for (int i = 0; i < numberOfStudents; i++) {
+			students[i] = input.nextInt();
+			if (students[i] > best) {
+				best = students[i];
+			}
+		}
+		input.close();
+		for (int i = 0; i < numberOfStudents; i++) {
+			System.out.print("Student " + i + " score is " + students[i] + " and grade is ");
+			char grade;
+			if (students[i] >= best - 10) {
+				grade = 'A';
+			} else if (students[i] >= best - 20) {
+				grade = 'B';
+			} else if (students[i] >= best - 30) {
+				grade = 'C';
+			} else if (students[i] >= best - 40) {
+				grade = 'D';
+			} else {
+				grade = 'F';
+			}
+			System.out.println(grade);
+		}
+	}
+
+}
