ca2f009a525b57d619e2adb0c8930aa5069b3d16
Viktor Bohush <vbohush@server.fake>
2013-12-19 01:57:13 +0200

    solve exercises 6.28 - 6.31

--- src/ua/pp/viktor/chapter06/Exercise24.java
+++ src/ua/pp/viktor/chapter06/Exercise24.java
@@ -2,53 +2,44 @@ package ua.pp.viktor.chapter06;
 
 public class Exercise24 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		int[] deck = new int[52];
+		String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+		String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+				"Jack", "Queen", "King" };
+
+		// Initialize cards
+		for (int i = 0; i < deck.length; i++)
+			deck[i] = i;
+
+		// Shuffle the cards
+		for (int i = 0; i < deck.length; i++) {
+			// Generate an index randomly
+			int index = (int) (Math.random() * deck.length);
+			int temp = deck[i];
+			deck[i] = deck[index];
+			deck[index] = temp;
+		}
+
 		boolean pickedAllCards = false;
 		boolean[] cardSuit = new boolean[4];
 		int count = 0;
 		while (!pickedAllCards) {
 			count++;
-			int card = getCard();
-			if (!cardSuit[getSuitOfCard(card)]) {
-				cardSuit[getSuitOfCard(card)] = true;
-				System.out.println(getStringOfCard(card));
+			
+			int card = (int)(Math.random() * deck.length);
+			String suit = suits[deck[card] / 13];
+			String rank = ranks[deck[card] % 13];
+			
+			if (!cardSuit[deck[card] / 13]) {
+				cardSuit[deck[card] / 13] = true;
+				System.out.println("Card number " + deck[card] + ": " + rank + " of " + suit);
 			}
 			pickedAllCards = cardSuit[0] && cardSuit[1] && cardSuit[2] && cardSuit[3];
 		}
 		System.out.println("Number of picks: " + count);
 	}
-	
-	public static int getCard() {
-		int rank = 1 + (int)(Math.random() * 13);
-		int suit = (int)(Math.random() * 4);
-		return rank*100 + suit;
-	}
-	
-	public static int getSuitOfCard(int card) {
-		return card % 100;
-	}
-	
-	public static String getStringOfCard(int card) {
-		int rank = card / 100;
-		int suit = card % 100;
-		String result = "";
-		switch (rank) {
-		case 1: result += "Ace"; break;
-		case 11: result += "Jack"; break;
-		case 12: result += "Queen"; break;
-		case 13: result += "King"; break;
-		default: result += rank; break;
-		}
-		result += " of ";
-		switch (suit) {
-		case 0: result += "Clubs"; break;
-		case 1: result += "Diamonds"; break;
-		case 2: result += "Hearts"; break;
-		case 3: result += "Spades"; break;
-		}
-		return result;
-	}
-	
+
 	
 
 }
