b1ab2d1d3c7dcd03ad0885bb002d105abc9af49f
Viktor Bohush <vbohush@server.fake>
2013-12-21 18:50:14 +0200

    solve exercises 7.11 - 7.15

---
+++ src/ua/pp/viktor/chapter07/Exercise12.java
@@ -0,0 +1,41 @@
+package ua.pp.viktor.chapter07;
+
+import java.util.Scanner;
+
+public class Exercise12 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("(0-single filer, 1-married jointly or qualifying widow(er), "
+						+ "\n2-married separately, 3-head of household)\n"
+						+ "Enter the filing status: ");
+		int status = input.nextInt();
+		System.out.print("Enter the taxable income: ");
+		double income = input.nextDouble();
+		input.close();
+		double[] rates = {0.10, 0.15, 0.25, 0.28, 0.33, 0.35};
+		int[][] brackets = {{8350, 33950, 82250, 171550, 372950}, // Single filer
+							{16700, 67900, 137050, 20885, 372950}, // Married jointly or qualifying widow(er)
+							{8350, 33950, 68525, 104425, 186475}, // Married separately
+							{11950, 45500, 117450, 190200, 372950}};
+		int i = 0;
+		double tax = 0;
+		while ((i < 5) && (brackets[status][i] < income)) {
+			if (i == 0) {
+				tax += brackets[status][i] * rates[i];
+			} else {
+				tax += (brackets[status][i] - brackets[status][i - 1]) * rates[i];				
+			}
+			i++;
+		}
+		if (i == 0) {
+			tax += income * rates[i];	
+		} else {
+			tax += (income - brackets[status][i - 1]) * rates[i];			
+		}
+		
+		System.out.println("Tax is " + tax);
+	}
+
+
+}
---
+++ src/ua/pp/viktor/chapter07/Exercise14.java
@@ -0,0 +1,106 @@
+package ua.pp.viktor.chapter07;
+
+import java.util.Scanner;
+
+public class Exercise14 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the size for the matrix: ");
+		int size = 4/*input.nextInt()*/;
+		input.close();
+		
+		
+		int array[][] = new int[size][size];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = (int)(Math.random() * 2);
+				System.out.print(array[i][j]);
+			}
+			System.out.println();
+		}
+		
+
+		if (!(printSameRows(0, array))&(!printSameRows(1, array))) {
+				System.out.println("No same numbers on a row");	
+		}
+
+		
+		if (!(printSameColumns(0, array))&(!printSameColumns(1, array))) {
+			System.out.println("No same numbers on a column");	
+		}
+		
+		if (!(printDiagonal(0, array))&(!printDiagonal(1, array))) {
+			System.out.println("No same numbers on the major diagonal");	
+		}
+		
+		if (!(printSubDiagonal(0, array))&(!printSubDiagonal(1, array))) {
+			System.out.println("No same numbers on the sub-diagonal");	
+		}
+		
+	}
+	
+	public static boolean printSubDiagonal(int number, int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i][array.length - i -1] != number) {
+				return false;
+			}
+		}
+		System.out.println("All " + number + "s on the sub-diagonal");
+		return true;
+	}
+	
+	public static boolean printDiagonal(int number, int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i][i] != number) {
+				return false;
+			}
+		}
+		System.out.println("All " + number + "s on the major diagonal");
+		return true;
+	}
+
+	public static boolean printSameColumns(int number, int[][] array) {
+		boolean isColumns = false;
+		for (int i = 0; i < array.length; i++) {
+			if (isColumnSame(number, i, array)) {
+				System.out.println("All " + number + "s on column " + i);
+				isColumns = true;
+			}
+		}
+		return isColumns;
+	}
+	
+	public static boolean isColumnSame(int number, int column, int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i][column] != number) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+
+	public static boolean printSameRows(int number, int[][] array) {
+		boolean isRows = false;
+		for (int i = 0; i < array.length; i++) {
+			if (isRowSame(number, i, array)) {
+				System.out.println("All " + number + "s on row " + i);
+				isRows = true;
+			}
+		}
+		return isRows;
+	}
+	
+	public static boolean isRowSame(int number, int row, int[][] array) {
+		for (int i = 0; i < array[row].length; i++) {
+			if (array[row][i] != number) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+
+}
