70710c06db246d64ef1601d2e1d409045cba52ed
Viktor Bohush <vbohush@server.fake>
2014-02-04 00:27:46 +0200

    solve exercise 19.02

---
+++ src/net/bohush/exercises/chapter19/Exercise02.java
@@ -0,0 +1,16 @@
+package net.bohush.exercises.chapter19;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Exercise02 {
+	public static void main(String[] args) throws IOException {
+		DataOutputStream output = new DataOutputStream(new FileOutputStream("tmp/Exercise19_02.txt", true));
+		for (int i = 0; i < 100; i++) {
+			output.writeInt((int)(Math.random() * Integer.MAX_VALUE));
+		}
+		output.close();
+	}
+	
+}
