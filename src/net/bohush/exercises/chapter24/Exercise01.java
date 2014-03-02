457da5eb796d919e4d7181fd2e50a2ae53b9d581
Viktor Bohush <vbohush@users.noreply.github.com>
2014-03-03 01:18:32 +0200

    solve exercise 24.01

---
+++ src/net/bohush/exercises/chapter24/Exercise01.java
@@ -0,0 +1,41 @@
+package net.bohush.exercises.chapter24;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String string = input.next();
+		char[] charString = string.toCharArray();
+		
+		LinkedList<Character> result = new LinkedList<>();
+		for (int j = 0; j < charString.length; j++) {
+			LinkedList<Character> tmpResult = new LinkedList<>();
+			tmpResult.add(charString[j]);
+			for (int i = j + 1; i < charString.length; i++) {
+				if(charString[i] > tmpResult.getLast()) {
+					tmpResult.add(charString[i]);
+				} else {
+					break;
+				}
+			}
+			if(result == null) {
+				result = tmpResult;				
+			} else {
+				if (tmpResult.size() > result.size()) {
+					result = tmpResult;
+				}
+			}						
+		}
+		for (Character character : result) {
+			System.out.print(character);
+		}
+		
+
+	}
+
+}
