af864a4e3674f7c130fe8d2edd2a5504f39f79ee
Viktor Bohush <vbohush@users.noreply.github.com>
2014-03-09 02:45:12 +0200

    solve exercise 24.18

--- src/net/bohush/exercises/chapter24/Exercise18.java
+++ src/net/bohush/exercises/chapter24/Exercise18.java
@@ -6,17 +6,16 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Collections;
 
-public class Exercise16 extends JApplet {
+public class Exercise18 extends JApplet {
 
 	private static final long serialVersionUID = 1L;
-	private LinearSearchPanel linearSearchPanel = new LinearSearchPanel();
+	private BinarySearchPanel binarySearchPanel = new BinarySearchPanel();
 	private JTextField jTextField = new JTextField("37", 5);
 	
-	public Exercise16() {
+	public Exercise18() {
 		setLayout(new BorderLayout());
-		add(linearSearchPanel, BorderLayout.CENTER);
+		add(binarySearchPanel, BorderLayout.CENTER);
 		
 		JPanel panel1 = new JPanel();
 		panel1.add(new JLabel("Key (in double): "));
@@ -33,16 +32,16 @@ public class Exercise16 extends JApplet {
 				try {
 					int value = Integer.parseInt(jTextField.getText());
 					jTextField.setEditable(false);
-					try {
-						if (linearSearchPanel.step(value)) {
+					if (binarySearchPanel.isValid(value)) {
+						if (binarySearchPanel.step(value)) {
 							JOptionPane.showMessageDialog(null, "Key found!", "Finish", JOptionPane.INFORMATION_MESSAGE);
-							linearSearchPanel.reset();
+							binarySearchPanel.reset();
 							jTextField.setEditable(true);
 						};	
-					} catch (IndexOutOfBoundsException e2) {
+					} else {
 						JOptionPane.showMessageDialog(null, "Key not found!", "Error", JOptionPane.ERROR_MESSAGE);
-						linearSearchPanel.reset();
-						jTextField.setEditable(true);
+						binarySearchPanel.reset();
+						jTextField.setEditable(true);						
 					}
 				} catch (NumberFormatException e2) {
 				}
@@ -53,18 +52,28 @@ public class Exercise16 extends JApplet {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				jTextField.setEditable(true);
-				linearSearchPanel.reset();
+				binarySearchPanel.reset();
 			}
 		});
 	}
 
-	static class LinearSearchPanel extends JPanel {
+	static class BinarySearchPanel extends JPanel {
 		private static final long serialVersionUID = 1L;
 		private int searchSize = 50;
+		private int startPosition = -1;
+		private int endPosition = searchSize;
 		private int stepPosition = -1;
 		ArrayList<Integer> numbers = new ArrayList<>();
 		
-		public LinearSearchPanel() {
+		public boolean isValid(int value) {
+			if ((value < numbers.get(0)) || (value > numbers.get(numbers.size() - 1))) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+		public BinarySearchPanel() {
 			for (int i = 1; i <= searchSize; i++) {
 				numbers.add(i);
 			}
@@ -72,13 +81,22 @@ public class Exercise16 extends JApplet {
 		}
 		
 		public boolean step(int value) {
-			stepPosition++;
+			if(stepPosition == -1) {
+				startPosition++;
+				endPosition--;
+			} else if(value > numbers.get(stepPosition)) {
+				startPosition = stepPosition + 1;
+			} else if(value < numbers.get(stepPosition)) {
+				endPosition = stepPosition - 1;
+			}  
+			stepPosition = (endPosition + startPosition) / 2;
 			repaint();
-			return value == numbers.get(stepPosition);			
+			return numbers.get(stepPosition) == value;			
 		}
 		
 		public void reset() {
-			Collections.shuffle(numbers);
+			startPosition = -1;
+			endPosition = searchSize;
 			stepPosition = -1;
 			repaint();
 		}
@@ -88,20 +106,26 @@ public class Exercise16 extends JApplet {
 			super.paintComponent(g);
 			int numberWidth = getWidth() / (searchSize + 2);
 			int numberHeight = getHeight() / (searchSize + 2);
+			if(stepPosition != -1) {
+				g.setColor(Color.LIGHT_GRAY);
+				for (int i = startPosition; i <= endPosition; i++) {
+					g.fillRect(numberWidth * (i + 1), getHeight() - numberHeight * numbers.get(i), numberWidth, numberHeight * numbers.get(i));
+				}
+				g.setColor(Color.BLACK);	
+				g.fillRect(numberWidth * (stepPosition + 1), getHeight() - numberHeight * numbers.get(stepPosition), numberWidth, numberHeight * numbers.get(stepPosition));
+			}
 			for (int i = 0; i < searchSize; i++) {
 				g.drawRect(numberWidth * (i + 1), getHeight() - numberHeight * numbers.get(i), numberWidth, numberHeight * numbers.get(i));
 				g.drawString(numbers.get(i) + "", numberWidth * (i + 1), getHeight() - numberHeight * numbers.get(i) - (int)(numberHeight * 0.5));
 			}
-			if((stepPosition != -1)&&(stepPosition < searchSize)) {
-				g.fillRect(numberWidth * (stepPosition + 1), getHeight() - numberHeight * numbers.get(stepPosition), numberWidth, numberHeight * numbers.get(stepPosition));
-			}
+
 		}
 
 	}
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Exercise16");
-		Exercise16 applet = new Exercise16();
+		JFrame frame = new JFrame("Exercise18");
+		Exercise18 applet = new Exercise18();
 		frame.add(applet);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(960, 500);
