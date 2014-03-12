b26018afe335aa38380e1b35abf46d6cbd071b14
Viktor Bohush <vbohush@users.noreply.github.com>
2014-03-13 00:23:56 +0200

    solve exercise 25.02

--- src/net/bohush/exercises/chapter25/Exercise02.java
+++ src/net/bohush/exercises/chapter25/Exercise02.java
@@ -1,23 +1,39 @@
 package net.bohush.exercises.chapter25;
 
+import java.util.Comparator;
+
 public class Exercise02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Integer[] array1 = new Integer[10];
+		for (int i = 0; i < array1.length; i++) {
+			array1[i] = (int)(Math.random() * 100000);
+		}
+		System.out.println(java.util.Arrays.toString(array1));
+		mergeSort(array1);
+		System.out.println(java.util.Arrays.toString(array1));
+		
+		Integer[] array2 = new Integer[10];
+		for (int i = 0; i < array2.length; i++) {
+			array2[i] = (int)(Math.random() * 100000);
+		}
+		System.out.println("\n\n" + java.util.Arrays.toString(array2));
+		mergeSort(array2, new IntegerComparator());
+		System.out.println(java.util.Arrays.toString(array2));
 	}
 
 	
+	@SuppressWarnings("unchecked")
 	public static <E extends Comparable<E>> void mergeSort(E[] list) {
 		if (list.length > 1) {
 			// Merge sort the first half
-			E[] firstHalf = new E[list.length / 2];
+			E[] firstHalf = (E[])new Comparable[list.length / 2];
 			System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
 			mergeSort(firstHalf);
 
 			// Merge sort the second half
 			int secondHalfLength = list.length - list.length / 2;
-			E[] secondHalf = new E[secondHalfLength];
+			E[] secondHalf = (E[])new Comparable[secondHalfLength];
 			System.arraycopy(list, list.length / 2, secondHalf, 0,	secondHalfLength);
 			mergeSort(secondHalf);
 
@@ -33,7 +49,7 @@ public class Exercise02 {
 		int current3 = 0; // Current index in temp
 
 		while (current1 < list1.length && current2 < list2.length) {
-			if (list1[current1] < list2[current2])
+			if (list1[current1].compareTo(list2[current2]) < 0)
 				temp[current3++] = list1[current1++];
 			else
 				temp[current3++] = list2[current2++];
@@ -47,38 +63,45 @@ public class Exercise02 {
 	}
 	
 	
-	public static<E>void mergeSort(E[] list, Comparator<? super E> comparator) {
+	
+	
+	
+	static class IntegerComparator implements Comparator<Integer> {
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			return o1.intValue() - o2.intValue();
+		}
 		
 	}
 	
-	/** The method for sorting the numbers */
-	public static void mergeSort(int[] list) {
+	@SuppressWarnings("unchecked")
+	public static<E>void mergeSort(E[] list, Comparator<? super E> comparator) {
 		if (list.length > 1) {
 			// Merge sort the first half
-			int[] firstHalf = new int[list.length / 2];
+			E[] firstHalf = (E[])new Comparable[list.length / 2];
 			System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
-			mergeSort(firstHalf);
+			mergeSort(firstHalf, comparator);
 
 			// Merge sort the second half
 			int secondHalfLength = list.length - list.length / 2;
-			int[] secondHalf = new int[secondHalfLength];
-			System.arraycopy(list, list.length / 2, secondHalf, 0,
-					secondHalfLength);
-			mergeSort(secondHalf);
+			E[] secondHalf = (E[])new Comparable[secondHalfLength];
+			System.arraycopy(list, list.length / 2, secondHalf, 0,	secondHalfLength);
+			mergeSort(secondHalf, comparator);
 
 			// Merge firstHalf with secondHalf into list
-			merge(firstHalf, secondHalf, list);
+			merge(firstHalf, secondHalf, list, comparator);
 		}
 	}
-
+	
+	
 	/** Merge two sorted lists */
-	public static void merge(int[] list1, int[] list2, int[] temp) {
+	public static <E> void merge(E[] list1, E[] list2, E[] temp, Comparator<? super E> comparator) {
 		int current1 = 0; // Current index in list1
 		int current2 = 0; // Current index in list2
 		int current3 = 0; // Current index in temp
 
 		while (current1 < list1.length && current2 < list2.length) {
-			if (list1[current1] < list2[current2])
+			if (comparator.compare(list1[current1], list2[current2]) < 0)
 				temp[current3++] = list1[current1++];
 			else
 				temp[current3++] = list2[current2++];
@@ -90,5 +113,5 @@ public class Exercise02 {
 		while (current2 < list2.length)
 			temp[current3++] = list2[current2++];
 	}
-
+	
 }
