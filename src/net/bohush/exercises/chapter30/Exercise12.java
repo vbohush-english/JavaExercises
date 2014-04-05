fce992675ee01b1a0e70151afd2faee13069aaeb
viktor <viktor@viktor-pc.(none)>
2014-04-05 22:36:37 +0300

    testing

--- src/net/bohush/exercises/chapter30/Exercise12.java
+++ src/net/bohush/exercises/chapter30/Exercise12.java
@@ -3,8 +3,7 @@ package net.bohush.exercises.chapter30;
 public class Exercise12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(1);
 	}
 
 }
