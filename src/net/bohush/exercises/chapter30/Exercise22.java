d6d83f51d86ae9f71adef2796b42b73b8f10244d
vbohush <vbohush@users.noreply.github.com>
2014-04-09 22:57:07 +0300

    solve exercise 30.22

---
+++ src/net/bohush/exercises/chapter30/Exercise22.java
@@ -0,0 +1,511 @@
+package net.bohush.exercises.chapter30;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Exercise22 extends JApplet {
+	private static final long serialVersionUID = 1L;
+	// Circles are stored in a list
+	private List<Circle> circles = new ArrayList<Circle>();
+	private int movedCircle;
+	private boolean isMoving = false;
+	private CirclePanel circlePanel = new CirclePanel();
+	private int dx;
+	private int dy;
+
+	public Exercise22() {
+		add(circlePanel); // Add to circle panel to applet
+	}
+
+	/** Panel for displaying circles */
+	class CirclePanel extends JPanel {
+		private static final long serialVersionUID = 1L;
+
+		public CirclePanel() {
+			addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					if (!isInsideACircle(e.getPoint())) { // Add a new circle
+						circles.add(new Circle(e.getX(), e.getY()));
+						repaint();
+					}
+				}
+				
+				@Override
+				public void mousePressed(MouseEvent e) {
+					int x = e.getX();
+					int y = e.getY();
+					for (int i = 0; i < circles.size(); i++) {
+						if(getLenght(circles.get(i).x, circles.get(i).y, x, y) < circles.get(i).radius) {
+							movedCircle = i;
+							isMoving = true;
+							dx = circles.get(i).x - x;
+							dy = circles.get(i).y - y;
+							break;
+						}
+					}
+				}
+				
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					isMoving = false;
+				}
+			});
+			
+			addMouseMotionListener(new MouseMotionAdapter() {				
+				@Override
+				public void mouseDragged(MouseEvent e) {
+					if(isMoving) {
+						circles.get(movedCircle).x = e.getX() + dx;
+						circles.get(movedCircle).y = e.getY() + dy;
+						circlePanel.repaint();
+					}
+				}
+			});
+		}
+
+		private double getLenght(double x1, double y1, double x2, double y2){
+			return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+		}
+		
+		/** Returns true if the point is inside an existing circle */
+		private boolean isInsideACircle(Point p) {
+			for (Circle circle : circles)
+				if (circle.contains(p))
+					return true;
+
+			return false;
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			if (circles.size() == 0)
+				return; // Nothing to paint
+
+			super.paintComponent(g);
+
+			// Build the edges
+			List<AbstractGraph.Edge> edges = new ArrayList<AbstractGraph.Edge>();
+			for (int i = 0; i < circles.size(); i++)
+				for (int j = i + 1; j < circles.size(); j++)
+					if (circles.get(i).overlaps(circles.get(j))) {
+						edges.add(new AbstractGraph.Edge(i, j));
+						edges.add(new AbstractGraph.Edge(j, i));
+					}
+
+			// Create a graph with circles as vertices
+			Graph<Circle> graph = new UnweightedGraph<Circle>(edges, circles);
+			AbstractGraph<Circle>.Tree tree = graph.dfs(0); // a DFS tree
+			boolean isAllCirclesConnected = circles.size() == tree
+					.getNumberOfVerticesFound();
+
+			for (Circle circle : circles) {
+				int radius = circle.radius;
+				if (isAllCirclesConnected) { // All circles are connected
+					g.setColor(Color.RED);
+					g.fillOval(circle.x - radius, circle.y - radius,
+							2 * radius, 2 * radius);
+				} else
+					// circles are not all connected
+					g.drawOval(circle.x - radius, circle.y - radius,
+							2 * radius, 2 * radius);
+			}
+		}
+	}
+
+	private static class Circle {
+		int radius = 20;
+		int x, y;
+
+		Circle(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public boolean contains(Point p) {
+			double d = distance(x, y, p.x, p.y);
+			return d <= radius;
+		}
+
+		public boolean overlaps(Circle circle) {
+			return distance(this.x, this.y, circle.x, circle.y) <= radius
+					+ circle.radius;
+		}
+
+		private static double distance(int x1, int y1, int x2, int y2) {
+			return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+		}
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JApplet applet = new Exercise22();
+		frame.add(applet);
+		frame.setTitle("Exercise22");
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600, 400);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	static class UnweightedGraph<V> extends AbstractGraph<V> {
+		/** Construct an empty graph */
+		public UnweightedGraph() {
+		}
+
+		/** Construct a graph from edges and vertices stored in arrays */
+		public UnweightedGraph(int[][] edges, V[] vertices) {
+			super(edges, vertices);
+		}
+
+		/** Construct a graph from edges and vertices stored in List */
+		public UnweightedGraph(List<Edge> edges, List<V> vertices) {
+			super(edges, vertices);
+		}
+
+		/** Construct a graph for integer vertices 0, 1, 2 and edge list */
+		public UnweightedGraph(List<Edge> edges, int numberOfVertices) {
+			super(edges, numberOfVertices);
+		}
+
+		/** Construct a graph from integer vertices 0, 1, and edge array */
+		public UnweightedGraph(int[][] edges, int numberOfVertices) {
+			super(edges, numberOfVertices);
+		}
+	}
+
+	static abstract class AbstractGraph<V> implements Graph<V> {
+		protected List<V> vertices = new ArrayList<V>(); // Store vertices
+		protected List<List<Integer>> neighbors = new ArrayList<List<Integer>>(); // Adjacency
+																					// lists
+
+		/** Construct an empty graph */
+		protected AbstractGraph() {
+		}
+
+		/** Construct a graph from edges and vertices stored in arrays */
+		protected AbstractGraph(int[][] edges, V[] vertices) {
+			for (int i = 0; i < vertices.length; i++)
+				this.vertices.add(vertices[i]);
+
+			createAdjacencyLists(edges, vertices.length);
+		}
+
+		/** Construct a graph from edges and vertices stored in List */
+		protected AbstractGraph(List<Edge> edges, List<V> vertices) {
+			for (int i = 0; i < vertices.size(); i++)
+				this.vertices.add(vertices.get(i));
+
+			createAdjacencyLists(edges, vertices.size());
+		}
+
+		/** Construct a graph for integer vertices 0, 1, 2 and edge list */
+		@SuppressWarnings("unchecked")
+		protected AbstractGraph(List<Edge> edges, int numberOfVertices) {
+			for (int i = 0; i < numberOfVertices; i++)
+				vertices.add((V) (new Integer(i))); // vertices is {0, 1, ...}
+
+			createAdjacencyLists(edges, numberOfVertices);
+		}
+
+		/** Construct a graph from integer vertices 0, 1, and edge array */
+		@SuppressWarnings("unchecked")
+		protected AbstractGraph(int[][] edges, int numberOfVertices) {
+			for (int i = 0; i < numberOfVertices; i++)
+				vertices.add((V) (new Integer(i))); // vertices is {0, 1, ...}
+
+			createAdjacencyLists(edges, numberOfVertices);
+		}
+
+		/** Create adjacency lists for each vertex */
+		private void createAdjacencyLists(int[][] edges, int numberOfVertices) {
+			// Create a linked list
+			for (int i = 0; i < numberOfVertices; i++) {
+				neighbors.add(new ArrayList<Integer>());
+			}
+
+			for (int i = 0; i < edges.length; i++) {
+				int u = edges[i][0];
+				int v = edges[i][1];
+				neighbors.get(u).add(v);
+			}
+		}
+
+		/** Create adjacency lists for each vertex */
+		private void createAdjacencyLists(List<Edge> edges, int numberOfVertices) {
+			// Create a linked list for each vertex
+			for (int i = 0; i < numberOfVertices; i++) {
+				neighbors.add(new ArrayList<Integer>());
+			}
+
+			for (Edge edge : edges) {
+				neighbors.get(edge.u).add(edge.v);
+			}
+		}
+
+		@Override
+		/** Return the number of vertices in the graph */
+		public int getSize() {
+			return vertices.size();
+		}
+
+		@Override
+		/** Return the vertices in the graph */
+		public List<V> getVertices() {
+			return vertices;
+		}
+
+		@Override
+		/** Return the object for the specified vertex */
+		public V getVertex(int index) {
+			return vertices.get(index);
+		}
+
+		@Override
+		/** Return the index for the specified vertex object */
+		public int getIndex(V v) {
+			return vertices.indexOf(v);
+		}
+
+		@Override
+		/** Return the neighbors of the specified vertex */
+		public List<Integer> getNeighbors(int index) {
+			return neighbors.get(index);
+		}
+
+		@Override
+		/** Return the degree for a specified vertex */
+		public int getDegree(int v) {
+			return neighbors.get(v).size();
+		}
+
+		@Override
+		/** Print the edges */
+		public void printEdges() {
+			for (int u = 0; u < neighbors.size(); u++) {
+				System.out.print(getVertex(u) + " (" + u + "): ");
+				for (int j = 0; j < neighbors.get(u).size(); j++) {
+					System.out.print("(" + u + ", " + neighbors.get(u).get(j)
+							+ ") ");
+				}
+				System.out.println();
+			}
+		}
+
+		@Override
+		/** Clear graph */
+		public void clear() {
+			vertices.clear();
+			neighbors.clear();
+		}
+
+		@Override
+		/** Add a vertex to the graph */
+		public void addVertex(V vertex) {
+			vertices.add(vertex);
+			neighbors.add(new ArrayList<Integer>());
+		}
+
+		@Override
+		/** Add an edge to the graph */
+		public void addEdge(int u, int v) {
+			neighbors.get(u).add(v);
+			neighbors.get(v).add(u);
+		}
+
+		/** Edge inner class inside the AbstractGraph class */
+		public static class Edge {
+			public int u; // Starting vertex of the edge
+			public int v; // Ending vertex of the edge
+
+			/** Construct an edge for (u, v) */
+			public Edge(int u, int v) {
+				this.u = u;
+				this.v = v;
+			}
+		}
+
+		@Override
+		/** Obtain a DFS tree starting from vertex v */
+		/** To be discussed in Section 27.6 */
+		public Tree dfs(int v) {
+			List<Integer> searchOrder = new ArrayList<Integer>();
+			int[] parent = new int[vertices.size()];
+			for (int i = 0; i < parent.length; i++)
+				parent[i] = -1; // Initialize parent[i] to -1
+
+			// Mark visited vertices
+			boolean[] isVisited = new boolean[vertices.size()];
+
+			// Recursively search
+			dfs(v, parent, searchOrder, isVisited);
+
+			// Return a search tree
+			return new Tree(v, parent, searchOrder);
+		}
+
+		/** Recursive method for DFS search */
+		private void dfs(int v, int[] parent, List<Integer> searchOrder,
+				boolean[] isVisited) {
+			// Store the visited vertex
+			searchOrder.add(v);
+			isVisited[v] = true; // Vertex v visited
+
+			for (int i : neighbors.get(v)) {
+				if (!isVisited[i]) {
+					parent[i] = v; // The parent of vertex i is v
+					dfs(i, parent, searchOrder, isVisited); // Recursive search
+				}
+			}
+		}
+
+		@Override
+		/** Starting bfs search from vertex v */
+		/** To be discussed in Section 27.7 */
+		public Tree bfs(int v) {
+			List<Integer> searchOrder = new ArrayList<Integer>();
+			int[] parent = new int[vertices.size()];
+			for (int i = 0; i < parent.length; i++)
+				parent[i] = -1; // Initialize parent[i] to -1
+
+			java.util.LinkedList<Integer> queue = new java.util.LinkedList<Integer>(); // list
+																						// used
+																						// as
+																						// a
+																						// queue
+			boolean[] isVisited = new boolean[vertices.size()];
+			queue.offer(v); // Enqueue v
+			isVisited[v] = true; // Mark it visited
+
+			while (!queue.isEmpty()) {
+				int u = queue.poll(); // Dequeue to u
+				searchOrder.add(u); // u searched
+				for (int w : neighbors.get(u)) {
+					if (!isVisited[w]) {
+						queue.offer(w); // Enqueue w
+						parent[w] = u; // The parent of w is u
+						isVisited[w] = true; // Mark it visited
+					}
+				}
+			}
+
+			return new Tree(v, parent, searchOrder);
+		}
+
+		/** Tree inner class inside the AbstractGraph class */
+		/** To be discussed in Section 27.5 */
+		public class Tree {
+			private int root; // The root of the tree
+			private int[] parent; // Store the parent of each vertex
+			private List<Integer> searchOrder; // Store the search order
+
+			/** Construct a tree with root, parent, and searchOrder */
+			public Tree(int root, int[] parent, List<Integer> searchOrder) {
+				this.root = root;
+				this.parent = parent;
+				this.searchOrder = searchOrder;
+			}
+
+			/** Return the root of the tree */
+			public int getRoot() {
+				return root;
+			}
+
+			/** Return the parent of vertex v */
+			public int getParent(int v) {
+				return parent[v];
+			}
+
+			/** Return an array representing search order */
+			public List<Integer> getSearchOrder() {
+				return searchOrder;
+			}
+
+			/** Return number of vertices found */
+			public int getNumberOfVerticesFound() {
+				return searchOrder.size();
+			}
+
+			/** Return the path of vertices from a vertex to the root */
+			public List<V> getPath(int index) {
+				ArrayList<V> path = new ArrayList<V>();
+
+				do {
+					path.add(vertices.get(index));
+					index = parent[index];
+				} while (index != -1);
+
+				return path;
+			}
+
+			/** Print a path from the root to vertex v */
+			public void printPath(int index) {
+				List<V> path = getPath(index);
+				System.out.print("A path from " + vertices.get(root) + " to "
+						+ vertices.get(index) + ": ");
+				for (int i = path.size() - 1; i >= 0; i--)
+					System.out.print(path.get(i) + " ");
+			}
+
+			/** Print the whole tree */
+			public void printTree() {
+				System.out.println("Root is: " + vertices.get(root));
+				System.out.print("Edges: ");
+				for (int i = 0; i < parent.length; i++) {
+					if (parent[i] != -1) {
+						// Display an edge
+						System.out.print("(" + vertices.get(parent[i]) + ", "
+								+ vertices.get(i) + ") ");
+					}
+				}
+				System.out.println();
+			}
+		}
+	}
+
+	interface Graph<V> {
+		/** Return the number of vertices in the graph */
+		public int getSize();
+
+		/** Return the vertices in the graph */
+		public java.util.List<V> getVertices();
+
+		/** Return the object for the specified vertex index */
+		public V getVertex(int index);
+
+		/** Return the index for the specified vertex object */
+		public int getIndex(V v);
+
+		/** Return the neighbors of vertex with the specified index */
+		public java.util.List<Integer> getNeighbors(int index);
+
+		/** Return the degree for a specified vertex */
+		public int getDegree(int v);
+
+		/** Print the edges */
+		public void printEdges();
+
+		/** Clear graph */
+		public void clear();
+
+		/** Add a vertex to the graph */
+		public void addVertex(V vertex);
+
+		/** Add an edge to the graph */
+		public void addEdge(int u, int v);
+
+		/** Obtain a depth-first search tree */
+		public AbstractGraph<V>.Tree dfs(int v);
+
+		/** Obtain a breadth-first search tree */
+		public AbstractGraph<V>.Tree bfs(int v);
+	}
+
+}
+
