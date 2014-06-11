d50b9ec29ee60844a367c3b8ebec40b74c72e497
Viktor Bohush <vbohush@users.noreply.github.com>
2014-06-12 00:51:28 +0300

    solve exercise 38.03

--- src/net/bohush/exercises/chapter38/Exercise03.java
+++ src/net/bohush/exercises/chapter38/Exercise03.java
@@ -3,26 +3,69 @@ package net.bohush.exercises.chapter38;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.*;
 
 public class Exercise03 extends JApplet {
 	private static final long serialVersionUID = 1L;
-	private JButton jButton1 = new JButton(new ImageIcon(getClass().getResource("image/1.gif")));
-	private JButton jButton2 = new JButton(new ImageIcon(getClass().getResource("image/2.gif")));
-	private JButton jButton3 = new JButton(new ImageIcon(getClass().getResource("image/3.gif")));
-	private JButton jButton4 = new JButton("A");
+	private JButton jButton1 = new JButton1();
+	private JButton jButton2 = new JButton2();
+	private JButton jButton3 = new JButton3();
+	private JButton jButton4 = new JButton4();
 	private DrawPanel drawPanel = new DrawPanel();
+	private Color color = new Color(0, 255, 255);
+	private Color usedColor = new Color(255, 0, 128);
+	
 	public Exercise03() {
 		setLayout(new BorderLayout(5, 5));
 		JToolBar jToolBar1 = new JToolBar("My Tool Bar");
-		Color color = new Color(0, 255, 255);
+		
 		jToolBar1.setBackground(color);
 		jToolBar1.setFloatable(false);
+		jButton1.setMinimumSize(new Dimension(30, 30));
 		jButton1.setBackground(color);
 		jButton2.setBackground(color);
 		jButton3.setBackground(color);
 		jButton4.setBackground(color);
-		jButton4.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 32));
-		jButton4.setForeground(Color.BLACK);
+		jButton1.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				drawPanel.setMode(DrawPanel.LINE);
+				jButton1.setBackground(usedColor);
+				jButton2.setBackground(color);
+				jButton3.setBackground(color);
+				jButton4.setBackground(color);
+			}
+		});
+		jButton2.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				drawPanel.setMode(DrawPanel.RECT);
+				jButton1.setBackground(color);
+				jButton2.setBackground(usedColor);
+				jButton3.setBackground(color);
+				jButton4.setBackground(color);
+			}
+		});
+		jButton3.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				drawPanel.setMode(DrawPanel.OVAL);
+				jButton1.setBackground(color);
+				jButton2.setBackground(color);
+				jButton3.setBackground(usedColor);
+				jButton4.setBackground(color);
+			}
+		});
+		jButton4.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				drawPanel.setMode(DrawPanel.TEXT);
+				jButton1.setBackground(color);
+				jButton2.setBackground(color);
+				jButton3.setBackground(color);
+				jButton4.setBackground(usedColor);
+			}
+		});
 		jToolBar1.add(jButton1);
 		jToolBar1.add(jButton2);
 		jToolBar1.add(jButton3);
@@ -47,6 +90,57 @@ public class Exercise03 extends JApplet {
 		public final static int TEXT = 4;
 		
 		private int mode = NONE;
+		private int startX = -1;
+		private int startY = -1;
+		private int finX = -1;
+		private int finY = -1;
+		private String text = "";
+		
+		public DrawPanel() {
+			addMouseListener(new MouseAdapter() {
+				
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					repaint();
+				}
+				
+				@Override
+				public void mousePressed(MouseEvent e) {
+					startX = e.getX();
+					startY = e.getY();	
+					finX = e.getX();
+					finY = e.getY();
+					if(mode == TEXT) {
+						text = "";
+						DrawPanel.this.requestFocus();	
+					}					
+					repaint();
+					
+				}				
+			});
+			
+			addMouseMotionListener(new MouseMotionAdapter() {
+				
+				@Override
+				public void mouseDragged(MouseEvent e) {
+					finX = e.getX();
+					finY = e.getY();
+					repaint();
+				}
+			});
+			
+			addKeyListener(new KeyAdapter() {
+				
+				@Override
+				public void keyReleased(KeyEvent e) {
+					if(mode == TEXT) {
+						text += e.getKeyChar();
+						repaint();
+					}
+				}
+
+			});
+		}
 		
 		public void setMode(int mode) {
 			if((mode < NONE) || (mode > TEXT)) {
@@ -55,13 +149,109 @@ public class Exercise03 extends JApplet {
 			this.mode = mode;
 		}
 		
+		
+		
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			
+				switch (mode) {
+				case LINE:
+					g.drawLine(startX, startY, finX, finY);
+					break;
+				case RECT:
+					if((startX <= finX)&&(startY <= finY)) {
+						g.drawRect(startX, startY, finX - startX, finY - startY);	
+					} else if((startX >= finX)&&(startY >= finY)) {
+						g.drawRect(finX, finY, startX - finX, startY - finY);	
+					} else if((startX > finX)&&(startY < finY)) {
+						g.drawRect(finX, startY, startX - finX, finY - startY);	
+					} else if((startX < finX)&&(startY > finY)) {
+						g.drawRect(startX, finY, finX - startX, startY - finY);	
+					}					
+					break;
+				case OVAL:
+					if((startX <= finX)&&(startY <= finY)) {
+						g.drawOval(startX, startY, finX - startX, finY - startY);	
+					} else if((startX >= finX)&&(startY >= finY)) {
+						g.drawOval(finX, finY, startX - finX, startY - finY);	
+					} else if((startX > finX)&&(startY < finY)) {
+						g.drawOval(finX, startY, startX - finX, finY - startY);	
+					} else if((startX < finX)&&(startY > finY)) {
+						g.drawOval(startX, finY, finX - startX, startY - finY);	
+					}	
+					break;
+				case TEXT:
+					g.fillOval(startX - 2, startY - 2, 4, 4);
+					g.drawString(text, startX + 2, startY - 2);
+					break;
+				default:
+					break;
+				}
 		}
 	}
 	
+	class JButton1 extends JButton {
+		private static final long serialVersionUID = 1L;
+		
+		public JButton1() {
+			super("    ");
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawLine(3, getHeight() - 3, getWidth() - 3, 3);
+		}
+		
+	}
+	
+	class JButton2 extends JButton {
+		private static final long serialVersionUID = 1L;
+		
+		public JButton2() {
+			super("    ");
+		}
+		
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawRect(3, 3, getWidth() - 6, getHeight() - 6);
+		}
+		
+	}
+	
+	class JButton3 extends JButton {
+		private static final long serialVersionUID = 1L;
+		
+		public JButton3() {
+			super("    ");
+		}
+		
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawOval(3, 3, getWidth() - 6, getHeight() - 6);
+		}		
+	}
+	
+	class JButton4 extends JButton {
+		private static final long serialVersionUID = 1L;
+		
+		public JButton4() {
+			super("    ");
+		}
+		
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 28));
+			g.setColor(Color.BLACK);
+			g.drawString("A", 3, getHeight() - 3);
+		}		
+	}
 	public static void main(String[] args) {
 		Exercise03 applet = new Exercise03();
 		JFrame frame = new JFrame();
