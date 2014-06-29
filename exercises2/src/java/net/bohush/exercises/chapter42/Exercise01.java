776a53800a176919a3d8dd5ae3f50adedb560939
vbohush <vbohush@users.noreply.github.com>
2014-06-29 23:56:40 +0300

    solve exercise 42.01

---
+++ exercises2/src/java/net/bohush/exercises/chapter42/Exercise01.java
@@ -0,0 +1,48 @@
+package net.bohush.exercises.chapter42;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Exercise01 extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Exercise42_01</title>");            
+            out.println("</head>");
+            out.println("<body><table border=\"1\">");
+            out.println("<tr><td>Number</td><td>Factorial</td><tr>");
+            for (int i = 0; i <= 10; i++) {
+                out.println("<tr><td>" + i + "</td><td>" + factorial(i) + "</td><tr>");
+            }
+            out.println("</table></body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    private static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+}
