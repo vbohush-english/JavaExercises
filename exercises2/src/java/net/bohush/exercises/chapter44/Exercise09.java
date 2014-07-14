41cbb24b3e7a3feaf87b2676fe1931b1624af0a1
vbohush <vbohush@users.noreply.github.com>
2014-07-15 01:39:06 +0300

    solve exercise 44.09

---
+++ exercises2/src/java/net/bohush/exercises/chapter44/Exercise09.java
@@ -0,0 +1,256 @@
+package net.bohush.exercises.chapter44;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class Exercise09 {
+
+    private Statement statement;
+    private Connection connection;
+
+    private String id = "";
+    private String lastName = "";
+    private String firstName = "";
+    private String mi = "";
+    private String address = "";
+    private String city = "";
+    private String state = "";
+    private String telephone = "";
+    private String errorMessage = "";
+        
+    public Exercise09() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/javabook", "scott", "tiger");
+            statement = connection.createStatement();
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(net.bohush.exercises.chapter42.Exercise13.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        
+        try {
+            ResultSet rset = statement.executeQuery("select * from Staff limit 1;");
+            if(rset.next()) {
+                id = rset.getString(1);
+                lastName = rset.getString(2);
+                firstName = rset.getString(3);
+                mi = rset.getString(4);
+                address = rset.getString(5);
+                city = rset.getString(6);
+                state = rset.getString(7);
+                telephone = rset.getString(8);
+                errorMessage = "";  
+            } else {
+                id = "";
+                resetData();
+                errorMessage = "Table is empty";
+            }                
+        } catch (SQLException ex) {
+            id = "";
+            resetData();
+            errorMessage = "Table is empty";
+        }        
+    }
+    
+    public void doView() {
+        if((id == null) || (id.equals(""))) {
+            id = "";
+            resetData();
+            errorMessage = "Enter ID";   
+        } else {
+            try {
+                ResultSet rset = statement.executeQuery("select * from Staff where id = " + id + " limit 1;");
+                if(rset.next()) {
+                    id = rset.getString(1);
+                    lastName = rset.getString(2);
+                    firstName = rset.getString(3);
+                    mi = rset.getString(4);
+                    address = rset.getString(5);
+                    city = rset.getString(6);
+                    state = rset.getString(7);
+                    telephone = rset.getString(8);
+                    errorMessage = "";            
+                } else {
+                    resetData();
+                    errorMessage = "ID " + id + " not found";   
+                }                
+            } catch (SQLException ex) {
+                resetData();
+                errorMessage = "ID " + id + " not found";      
+            }
+        }        
+    }
+    
+    public void doInsert() {
+        if((id == null) || (id.equals(""))) {
+            errorMessage = "Enter ID";  
+        } else {
+            String queryString = "insert into Staff (id, lastName, firstName, mi, address, city, state, telephone) values (?, ?, ?, ?, ?, ?, ?, ?);";
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement(queryString);
+                preparedStatement.setString(1, id);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setString(3, firstName);
+                preparedStatement.setString(4, mi);
+                preparedStatement.setString(5, address);
+                preparedStatement.setString(6, city);
+                preparedStatement.setString(7, state);
+                preparedStatement.setString(8, telephone);
+                preparedStatement.executeUpdate();
+                errorMessage = "";   
+            } catch (SQLException e2) {
+                errorMessage = e2.getMessage();   
+            }
+        }        
+    }
+    
+    public void doUpdate() {
+        if((id == null) || (id.equals(""))) {
+           resetData();
+           errorMessage = "ID " + id + " not found";   
+        } else {
+           String queryString = "update Staff set lastName = ?, firstName = ?, mi = ?, address = ?, city = ?, state = ?, telephone = ? where id = ?";
+           try {
+               PreparedStatement preparedStatement = connection.prepareStatement(queryString);
+               preparedStatement.setString(1, lastName);
+               preparedStatement.setString(2, firstName);
+               preparedStatement.setString(3, mi);
+               preparedStatement.setString(4, address);
+               preparedStatement.setString(5, city);
+               preparedStatement.setString(6, state);
+               preparedStatement.setString(7, telephone);
+               preparedStatement.setString(8, id);
+               preparedStatement.executeUpdate();
+               errorMessage = "";  
+           } catch (SQLException e2) {
+               errorMessage = e2.getMessage();   
+           }
+        }       
+    }
+    
+    public void doDelete() {
+        if((id == null) || (id.equals(""))) {
+            id = "";
+            resetData();
+            errorMessage = "Enter ID"; 
+        } else {
+            try {
+                statement.executeUpdate("delete from Staff where id = " + id + "");
+                try {
+                    ResultSet rset = statement.executeQuery("select * from Staff limit 1;");
+                    if (rset.next()) {
+                        id = rset.getString(1);
+                        lastName = rset.getString(2);
+                        firstName = rset.getString(3);
+                        mi = rset.getString(4);
+                        address = rset.getString(5);
+                        city = rset.getString(6);
+                        state = rset.getString(7);
+                        telephone = rset.getString(8);
+                        errorMessage = "";  
+                    } else {
+                        id = "";
+                        resetData();
+                        errorMessage = "Table is empty";
+                    }
+                } catch (SQLException ex) {
+                    id = "";
+                    resetData();
+                    errorMessage = "Table is empty";        
+                }
+            } catch (SQLException ex) {
+                id = "";
+                resetData();
+                errorMessage = ex.getMessage();   
+            }
+        }        
+    }
+    
+    private void resetData() {
+        lastName = "";
+        firstName = "";
+        mi = "";
+        address = "";
+        city = "";
+        state = "";
+        telephone = "";
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMi() {
+        return mi;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setMi(String mi) {
+        this.mi = mi;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    
+}
