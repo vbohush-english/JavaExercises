9dfaa5a5fca9acb80ce34126f0548d9cc5156a7e
vbohush <vbohush@users.noreply.github.com>
2014-08-09 14:13:05 +0300

    solve exercise 49.14

--- exercises1/src/net/bohush/exercises/chapter49/Exercise14.java
+++ exercises1/src/net/bohush/exercises/chapter49/Exercise14.java
@@ -1,17 +1,20 @@
 package net.bohush.exercises.chapter49;
 
-import java.awt.BasicStroke;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.HeadlessException;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Exercise13 extends JFrame{
+public class Exercise14 extends JFrame{
 
-    public Exercise13() throws HeadlessException {
+    public Exercise14() throws HeadlessException {
         setLayout(new BorderLayout());
         add(new CylinderPanel(), BorderLayout.CENTER);
     }
@@ -22,25 +25,29 @@ public class Exercise13 extends JFrame{
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             Graphics2D g2d = (Graphics2D)g;
-
+            g2d.setColor(Color.BLACK);
             g2d.translate(getWidth() / 2, getHeight() / 2);
             
             int hSize = getHeight() / 4;
             int wSize = getWidth() / 4;
             
-            g2d.drawLine(- wSize, - hSize, - wSize, hSize);
-            g2d.drawLine(wSize, - hSize, wSize, hSize);
+            Shape shape1 = new Rectangle2D.Double(- wSize, - hSize, wSize * 2, hSize * 2);
+            Shape shape2 = new Ellipse2D.Double(- wSize, - hSize - (wSize / 4), wSize * 2, wSize / 2);
+            Shape shape3 = new Ellipse2D.Double(- wSize, hSize - (wSize / 4), wSize * 2, wSize / 2);
+
+            g2d.fill(shape3);
+            g2d.fill(shape1);
             
-            g2d.drawOval(- wSize, - hSize - (wSize / 4), wSize * 2, wSize / 2);
-            g2d.drawArc(- wSize, hSize - (wSize / 4), wSize * 2, wSize / 2, 180, 180);
-            g2d.setStroke(new BasicStroke(1.0f, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_ROUND, 1.0f, new float[]{10}, 0));
-            g2d.drawArc(- wSize, hSize - (wSize / 4), wSize * 2, wSize / 2, 0, 180);
+            g2d.setColor(g2d.getBackground());
+            g2d.fill(shape2);
+            g2d.setColor(Color.BLACK);
+            g2d.draw(shape2);
 
         }
     }
     
     public static void main(String[] args) {
-        JFrame jFrame = new Exercise13();
+        JFrame jFrame = new Exercise14();
         jFrame.setTitle(jFrame.getClass().getName());
         jFrame.setMinimumSize(new Dimension(200, 200));
         jFrame.setSize(600, 600);
