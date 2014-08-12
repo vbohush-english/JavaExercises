f4eda45757e53ef5683366af79f9d6c6ab92e067
vbohush <vbohush@users.noreply.github.com>
2014-08-12 19:46:19 +0300

    improve some exercises

--- exercises1/src/net/bohush/exercises/chapter15/Exercise20.java
+++ exercises1/src/net/bohush/exercises/chapter15/Exercise20.java
@@ -1,6 +1,7 @@
 package net.bohush.exercises.chapter15;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
@@ -8,58 +9,69 @@ import javax.swing.JPanel;
 
 public class Exercise20 extends JFrame {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public Exercise20() {
-		add(new MandelbrotCanvas());
-	}
+    public Exercise20() {
+        add(new MandelbrotCanvas());
+    }
 
-	public static void main(String[] args) {
-		Exercise20 frame = new Exercise20();
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		frame.setSize(500, 500);
-		frame.setLocationRelativeTo(null);
-		frame.setTitle("Exercise20");
-		frame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        Exercise20 frame = new Exercise20();
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("Exercise20");
+        frame.setVisible(true);
+    }
 
 }
 
 class MandelbrotCanvas extends JPanel {
 
-	private static final long serialVersionUID = 1L;
-	final static int COUNT_LIMIT = 60;
+    private static final long serialVersionUID = 1L;
+    final static int COUNT_LIMIT = 60;
+    private final int COLOR_R = 77;
+    private final int COLOR_G = 58;
+    private final int COLOR_B = 159;
+        
+    @Override
+    /**
+     * Paint a Mandelbrot image
+     */
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
-	@Override
-	/** Paint a Mandelbrot image */
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
+        for (double x = -2.0; x < 2.0; x += 0.005) {
+            for (double y = -2.0; y < 2.0; y += 0.005) {
+                int c = count(new Complex(x, y));
+                if (c == COUNT_LIMIT) {
+                    g.setColor(Color.BLACK); // c is in a Mandelbrot set
+                } else {
+                    g.setColor(new Color(c * COLOR_R % 256, c * COLOR_G % 256, c * COLOR_B % 256));
+                }
+                g.drawRect((int) (x * 200) + 400, (int) (y * 200) + 400, 1, 1); // Fill a tiny rectangle with the specified color
+            }
+        }
+    }
 
-		for (double x = -2.0; x < 2.0; x += 0.01) {
-			for (double y = -2.0; y < 2.0; y += 0.01) {
-				int c = count(new Complex(x, y));
-				if (c == COUNT_LIMIT) {
-					g.setColor(Color.BLACK);
-				} else {
-					g.setColor(new Color(c * 77 % 256, c * 58 % 256, c * 159 % 256));
-				}
-				g.drawRect((int) (x * 100) + 200, (int) (y * 100) + 200, 1, 1);
-			}
-		}
-	}
-
-	/** Return the iteration count */
-	int count(Complex c) {
-		Complex z = new Complex(0, 0); // z0
-
-		for (int i = 0; i < COUNT_LIMIT; i++) {
-			z = z.multiply(z).add(c); // Get z1, z2, . . .
-			if (z.abs() > 2) {
-				return i; // The sequence is unbounded
-			}
-		}
-
-		return COUNT_LIMIT; // Indicate a bounded sequence
-	}
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(800, 800);
+    }
+    
+    /**
+     * Return the iteration count
+     */
+    static int count(Complex c) {
+        Complex z = new Complex(0, 0);
+        z = z.multiply(z).add(c);
+        for (int i = 0; i < COUNT_LIMIT; i++) {
+            z = z.multiply(z).add(c);
+            if (z.abs() > 2) {
+                return i;
+            }
+        }
+        return COUNT_LIMIT;
+    }
 
 }
--- exercises1/src/net/bohush/exercises/chapter15/Exercise21.java
+++ exercises1/src/net/bohush/exercises/chapter15/Exercise21.java
@@ -1,65 +1,77 @@
 package net.bohush.exercises.chapter15;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Exercise21 extends JFrame{
+public class Exercise21 extends JFrame {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public Exercise21() {
-		add(new JuliaSet());
-	}
-	
-	public static void main(String[] args) {
-		Exercise21 frame = new Exercise21();
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		frame.setSize(500, 500);
-		frame.setLocationRelativeTo(null);
-		frame.setTitle("Exercise21");
-		frame.setVisible(true);
-	}
+    public Exercise21() {
+        add(new JuliaSet());
+    }
+
+    public static void main(String[] args) {
+        Exercise21 frame = new Exercise21();
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("Exercise20");
+        frame.setVisible(true);
+    }
 
 }
 
 class JuliaSet extends JPanel {
 
-	private static final long serialVersionUID = 1L;
-	final static int COUNT_LIMIT = 60;
+    private static final long serialVersionUID = 1L;
+    final static int COUNT_LIMIT = 60;
+    private final int COLOR_R = (int)(Math.random() * 256);
+    private final int COLOR_G = (int)(Math.random() * 256);
+    private final int COLOR_B = (int)(Math.random() * 256);
 
-	@Override
-	/** Paint a Mandelbrot image */
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
+    
+    @Override
+    /**
+     * Paint a Mandelbrot image
+     */
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
-		for (double x = -2.0; x < 2.0; x += 0.01) {
-			for (double y = -2.0; y < 2.0; y += 0.01) {
-				int c = count(new Complex(x, y));
-				if (c == COUNT_LIMIT) {
-					g.setColor(Color.BLACK);
-				} else {
-					g.setColor(new Color(c * 77 % 256, c * 58 % 256, c * 159 % 256));
-				}
-				g.drawRect((int) (x * 100) + 200, (int) (y * 100) + 200, 1, 1);
-			}
-		}
-	}
+        for (double x = -2.0; x < 2.0; x += 0.005) {
+            for (double y = -2.0; y < 2.0; y += 0.005) {
+                int c = count(new Complex(x, y));
+                if (c == COUNT_LIMIT) {
+                    g.setColor(Color.BLACK); // c is in a Mandelbrot set
+                } else {
+                    g.setColor(new Color(c * COLOR_R % 256, c * COLOR_G % 256, c * COLOR_B % 256));
+                }
+                g.drawRect(((int) (x * 200) + 400), (int) (y * 200) + 400, 1, 1); // Fill a tiny rectangle with the specified color
+            }
+        }
+    }
 
-	/** Return the iteration count */
-	static int count(Complex c) {
-		Complex z = c; // z0
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(800, 800);
+    }
+    
+    /**
+     * Return the iteration count
+     */
+    static int count(Complex z) {
+        Complex c = new Complex(-0.3, 0.6);
+        for (int i = 0; i < COUNT_LIMIT; i++) {
+            z = z.multiply(z).add(c);
+            if (z.abs() > 4) {
+                return i;
+            }
+        }
+        return COUNT_LIMIT;
+    }
 
-		for (int i = 0; i < COUNT_LIMIT; i++) {
-			z = z.multiply(z).add(new Complex(-0.3, 0.6)); // Get z1, z2, . . .
-			if (z.abs() > 2) {
-				return i; // The sequence is unbounded
-			}
-		}
-
-		return COUNT_LIMIT; // Indicate a bounded sequence
-	}
 }
-
